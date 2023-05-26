a3a3b8f72736000c04a4f898b32de1c51ad1fa0f
syed zunair gillani <gillanix007@gmail.com>
2023-05-26 17:48:12 +0500

    up

--- src/app/home/home2/page.tsx
+++ src/app/home/home2/page.tsx
@@ -10,7 +10,9 @@ import Image from "next/image";
 import { categoriesDate } from "@/const/categories";
 import Link from "next/link";
 import PostDesign from "@/components/post-design/post-design";
-import VideosGallery from '@/components/videos-gallery/videos'
+import VideosGallery from "@/components/videos-gallery/videos";
+import Footer2 from "@/components/footer/Footer2";
+import { BsArrowRight } from "react-icons/bs";
 
 const Home2 = () => {
   return (
@@ -52,18 +54,55 @@ const Home2 = () => {
           </h2>
           <section className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-20">
             {PostMokeData.slice(0, 6).map((post, idx) => {
-              return <PostDesign post={post} idx={idx} key={idx} rounded={true}/>;
+              return (
+                <PostDesign post={post} idx={idx} key={idx} rounded={true} />
+              );
             })}
           </section>
           <h2 className="text-3xl mt-20 uppercase text-center font-oswald">
-          🎬 The Videos
+            🎬 The Videos
           </h2>
           <section className="mt-20 p-4 md:p-10 bg-light-gray rounded-[40px]">
-            <VideosGallery/>
+            <VideosGallery />
           </section>
-          
+          <section className="mt-20">
+            <div className="grid grid-cols-1 items-center gap-10 px-10 mt-10 lg:grid-cols-2 ">
+              <div className="mt-20 ">
+                <h1 className="mb-10 text-5xl font-bold">
+                  Join our newsletter
+                </h1>
+                <p className="text-xl font-medium">
+                  Read and share new perspectives on just about any topic.
+                  Everyone’s welcome.
+                </p>
+                <div className="flex items-center mt-6">
+                  <h5 className="px-4 py-2 bg-yellow rounded-3xl">1</h5>
+                  <p className="px-8">Get more discount</p>
+                </div>
+                <div className="flex items-center mt-4">
+                  <h5 className="px-4 py-2 bg-light-blue rounded-3xl">2</h5>
+                  <p className="px-8">Get premium magazines</p>
+                </div>
+                <div className="mt-10 w-72">
+                  <div className="relative h-12 w-full max-w-[400px] lg:w-[400px] border border-border rounded-full">
+                    <div className="absolute right-0 flex p-1 w-10 h-10 rounded-full mr-1 mt-[3px] flex-col justify-center items-center bg-black active:scale-105 text-xl dark:bg-light-gray">
+                      <BsArrowRight color="white" />
+                    </div>
+                    <input
+                      className="peer h-full rounded-3xl border-t-transparent bg-transparent p-3 pb-3.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all  focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50  "
+                      placeholder="Enter your email "
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <img src="/assets/images/subscribe.webp" alt="" />
+              </div>
+            </div>
+          </section>
         </Layout>
       </div>
+      <Footer2/>
     </>
   );
 };
